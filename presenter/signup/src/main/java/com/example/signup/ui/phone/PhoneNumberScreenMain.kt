28f8d3e9ccc0957d2bce4bc485a98d1f9ffbdea5
sonny8569 <sonny132@naver.com>
2025-03-30 17:26:28 +0900

    문자인증 제외

--- presenter/signup/src/main/java/com/example/signup/ui/phone/PhoneNumberScreenMain.kt
+++ presenter/signup/src/main/java/com/example/signup/ui/phone/PhoneNumberScreenMain.kt
@@ -54,13 +54,10 @@ internal fun PhoneNumberScreenMain(
     activity: Activity,
 ) {
     val phoneNumber by viewModel.phoneNumber.collectAsState()
-    val smsCode by viewModel.smsCode.collectAsState()
-    val smsViewShow by viewModel.smsViewShow.collectAsState()
     val firebaseSMSState by viewModel.firebaseSMSState.collectAsState()
-    val smsTimer by viewModel.smsTime.collectAsState()
-
     var errorDialog by remember { mutableStateOf(false) }
     var alreadySignUpDialog by remember { mutableStateOf(false) }
+
     val buttonIsEnable = when (firebaseSMSState) {
         is SignUpViewModel.Action.VerifyFinish -> true
         else -> false
@@ -105,7 +102,6 @@ internal fun PhoneNumberScreenMain(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
-                .padding(bottom = 70.dp) // 버튼 높이만큼 패딩 추가
         ) {
             Text(
                 text = stringResource(R.string.txt_sign_up_title),
@@ -136,151 +132,42 @@ internal fun PhoneNumberScreenMain(
                 hint = stringResource(R.string.hint_input_phone_number)
             )
 
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.weight(1f))
 
             Button(
-                onClick = { viewModel.smsRequest(phoneNumber, activity) },
-                enabled = phoneNumber.isNotEmpty(),
+                onClick = { buttonClick() },
+                enabled = true,
                 shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(
+                        R.color.purple
+                    ),
+                ),
                 modifier = Modifier
-                    .width(120.dp)
-                    .height(36.dp)
-                    .align(Alignment.End)
+                    .fillMaxWidth()
+                    .height(50.dp)
             ) {
                 Text(
-                    text = stringResource(R.string.btn_send_message),
-                    fontSize = 14.sp,
-                    lineHeight = 20.sp,
+                    text = stringResource(R.string.btn_next),
+                    fontSize = 20.sp,
+                    lineHeight = 28.sp,
                     fontFamily = FontFamily(Font(R.font.medium)),
-                    fontWeight = FontWeight(500),
-                    color = colorResource(R.color.purple),
+                    fontWeight = FontWeight(600),
                     textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth(),
+                    color = colorResource(R.color.white)
                 )
             }
 
-            Spacer(modifier = Modifier.height(24.dp))
-
-            if (smsViewShow) {
-                CustomTextField(
-                    text = smsCode,
-                    modifier = Modifier.fillMaxWidth(),
-                    onValueChange = { number ->
-                        viewModel.signCodeNumber(number)
-                    },
-                    inputType = KeyboardType.Number,
-                    hint = stringResource(R.string.hint_sms_num),
-                    timeCount = smsTimer
+            if (alreadySignUpDialog) {
+                CustomDialog(
+                    onDismiss = { alreadySignUpDialog = false },
+                    buttonClick = { alreadySignUpDialog = false },
+                    titleText = stringResource(R.string.dialog_title_already_signup),
+                    contentText = stringResource(R.string.dialog_content_already_signup),
+                    buttonText = stringResource(R.string.dialog_btn_already_signup)
                 )
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = stringResource(R.string.txt_sms_not_received),
-                            fontSize = 12.sp,
-                            lineHeight = 18.sp,
-                            fontFamily = FontFamily(Font(R.font.medium)),
-                            fontWeight = FontWeight(400),
-                            color = colorResource(R.color.dark_gray)
-                        )
-
-                        Spacer(modifier = Modifier.width(4.dp))
-
-                        Text(
-                            text = stringResource(R.string.txt_resend_sms),
-                            fontSize = 12.sp,
-                            lineHeight = 18.sp,
-                            fontFamily = FontFamily(Font(R.font.medium)),
-                            fontWeight = FontWeight(600),
-                            color = Color.Red,
-                            modifier = Modifier.clickable {
-                                viewModel.smsRequest(phoneNumber, activity)
-                            }
-                        )
-                    }
-
-                    Button(
-                        onClick = { viewModel.sendCode(smsCode) },
-                        enabled = smsCode.isNotEmpty(),
-                        shape = RoundedCornerShape(10.dp),
-                        modifier = Modifier
-                            .width(90.dp)
-                            .height(36.dp)
-                    ) {
-                        Text(
-                            text = stringResource(R.string.btn_check_sms_num),
-                            fontSize = 14.sp,
-                            lineHeight = 20.sp,
-                            fontFamily = FontFamily(Font(R.font.medium)),
-                            fontWeight = FontWeight(500),
-                            color = colorResource(R.color.purple),
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
             }
-        }
-
-
-        Button(
-            onClick = { buttonClick() },
-            enabled = buttonIsEnable,
-            shape = RoundedCornerShape(10.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = colorResource(if(buttonIsEnable){
-                    R.color.purple
-                }else{
-                    R.color.bar_light_gray
-                }),
-            ),
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth()
-                .height(50.dp)
-        ) {
-            Text(
-                text = stringResource(R.string.btn_next),
-                fontSize = 20.sp,
-                lineHeight = 28.sp,
-                fontFamily = FontFamily(Font(R.font.medium)),
-                fontWeight = FontWeight(600),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth(),
-                color = colorResource(R.color.white)
-            )
-        }
-
-        // 다이얼로그 처리
-        if (errorDialog) {
-            CustomDialog(
-                onDismiss = { errorDialog = false },
-                buttonClick = {
-                    errorDialog = false
-                    viewModel.smsRequest(phoneNumber, activity)
-                },
-                titleText = stringResource(R.string.dialog_title_again_input),
-                contentText = stringResource(R.string.dialog_content_again_input),
-                buttonText = stringResource(R.string.dialog_btn_again_input)
-            )
-        }
-
-        if (alreadySignUpDialog) {
-            CustomDialog(
-                onDismiss = { alreadySignUpDialog = false },
-                buttonClick = { alreadySignUpDialog = false },
-                titleText = stringResource(R.string.dialog_title_already_signup),
-                contentText = stringResource(R.string.dialog_content_already_signup),
-                buttonText = stringResource(R.string.dialog_btn_already_signup)
-            )
         }
     }
 }
