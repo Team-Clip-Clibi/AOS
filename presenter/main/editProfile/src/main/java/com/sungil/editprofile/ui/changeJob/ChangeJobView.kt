0235a8b1e79827b545f359d799f9b9a46b4d2774
SUNGIL KIM <sonny0204@partron.co.kr>
2025-04-09 16:55:22 +0900

    snack bar message 대기

--- presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeJob/ChangeJobView.kt
+++ presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeJob/ChangeJobView.kt
@@ -1,28 +1,49 @@
 package com.sungil.editprofile.ui.changeJob
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.sungil.editprofile.ProfileEditViewModel
 import com.sungil.editprofile.R
 import com.sungil.editprofile.ui.CustomChangeDataAppBar
+import com.sungil.editprofile.ui.CustomSnackBar
 
 @Composable
 internal fun ChangeJobView(
     viewModel: ProfileEditViewModel,
     onBackClick: () -> Unit,
 ) {
+    val snackBarHostState = remember { SnackbarHostState() }
     Scaffold(
         topBar = {
             CustomChangeDataAppBar(
                 text = stringResource(R.string.top_job_change_job),
                 onBackClick = { onBackClick() }
             )
+        },
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackBarHostState,
+                snackbar = { data ->
+                    CustomSnackBar(data)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 17.dp , end = 16.dp , bottom = 91.dp)
+            )
         }
     ) { paddingValues ->
         ChangeJobMainView(
             paddingValues = paddingValues,
-            viewModel = viewModel
+            viewModel = viewModel,
+            snackBarHost = snackBarHostState
         )
     }
 }
