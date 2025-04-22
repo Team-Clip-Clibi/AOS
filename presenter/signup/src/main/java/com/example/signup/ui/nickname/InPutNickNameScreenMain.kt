0eb38c5f145df9b7cf47ffec5c729cedc455ff9a
sonny8569 <sonny132@naver.com>
2025-04-23 00:38:22 +0900

    배경색 추가

--- presenter/signup/src/main/java/com/example/signup/ui/nickname/InPutNickNameScreenMain.kt
+++ presenter/signup/src/main/java/com/example/signup/ui/nickname/InPutNickNameScreenMain.kt
@@ -1,6 +1,7 @@
 package com.example.signup.ui.nickname
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
@@ -27,7 +27,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -105,6 +104,7 @@ internal fun InPutNickNameScreenMain(
                 top = paddingValues.calculateTopPadding() + 32.dp,
                 bottom = 8.dp
             )
+            .background(color = Color(0xFFFFFFFF))
             .navigationBarsPadding()
     ) {
         Column(
