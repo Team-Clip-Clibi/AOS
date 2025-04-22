2006db07c53171038aa86f71139188e3ee3d224b
sonny8569 <sonny132@naver.com>
2025-04-23 00:35:44 +0900

    배경색 누락 수정

--- presenter/signup/src/main/java/com/example/signup/ui/phone/PhoneNumberScreenMain.kt
+++ presenter/signup/src/main/java/com/example/signup/ui/phone/PhoneNumberScreenMain.kt
@@ -1,22 +1,17 @@
 package com.example.signup.ui.phone
 
-import android.app.Activity
 import android.util.Log
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
@@ -24,29 +19,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material3.Button
-import androidx.wear.compose.material3.ButtonDefaults
 import com.example.core.AppTextStyles
 import com.example.signup.ERROR_ALREADY_SIGN_UP
 import com.example.signup.R
 import com.example.signup.SignUpViewModel
 import com.example.signup.ui.component.CustomButton
-import com.example.signup.ui.component.CustomDialog
 import com.example.signup.ui.component.CustomTextField
 import kotlinx.coroutines.flow.collectLatest
 
@@ -91,6 +74,7 @@ internal fun PhoneNumberScreenMain(
                 top = paddingValues.calculateTopPadding() + 32.dp,
                 bottom = 8.dp
             )
+            .background(color = Color(0xFFFFFFFF))
             .navigationBarsPadding()
     ) {
         Column(
