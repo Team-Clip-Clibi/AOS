53266bbba3fbe25e07fbc458796f800ff42be2fd
sonny8569 <sonny132@naver.com>
2025-04-23 21:05:47 +0900

    1.0.1_dev 개발버전 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.1_dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
