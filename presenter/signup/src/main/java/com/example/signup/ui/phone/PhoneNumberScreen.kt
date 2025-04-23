75de7daf699dc2b50386f35f404ed695c8b70614
SUNGIL KIM <sonny0204@partron.co.kr>
2025-04-23 15:51:07 +0900

    메세지 height 수정

--- presenter/signup/src/main/java/com/example/signup/ui/phone/PhoneNumberScreen.kt
+++ presenter/signup/src/main/java/com/example/signup/ui/phone/PhoneNumberScreen.kt
@@ -1,13 +1,24 @@
 package com.example.signup.ui.phone
 
 import android.app.Activity
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.signup.R
 import com.example.signup.SignUpViewModel
+import com.example.signup.ui.component.CustomSnackBar
 import com.example.signup.ui.component.TopBar
 
 
@@ -17,7 +28,9 @@ internal fun PhoneNumberScreen(
     actionClick: () -> Unit,
     buttonClick: () -> Unit,
     signUpPage: () -> Unit,
+    goToLoginPage: () -> Unit,
 ) {
+    val snackBarHostState = remember { SnackbarHostState() }
     Scaffold(
         topBar = {
             TopBar(
@@ -28,8 +41,33 @@ internal fun PhoneNumberScreen(
                     actionClick()
                 }
             )
+        },
+
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackBarHostState,
+                snackbar = { data ->
+                    CustomSnackBar(data)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 17.dp,
+                        end = 16.dp,
+                        bottom = WindowInsets.navigationBars
+                            .asPaddingValues()
+                            .calculateTopPadding() + 70.dp
+                    )
+            )
         }
     ) { paddingValues ->
-        PhoneNumberScreenMain(paddingValues, viewModel, buttonClick, signUpPage)
+        PhoneNumberScreenMain(
+            paddingValues,
+            viewModel,
+            buttonClick,
+            signUpPage,
+            snackBarHostState,
+            goToLoginPage
+        )
     }
 }
