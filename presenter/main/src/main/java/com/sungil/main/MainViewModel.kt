28ab36e4626b148d177335d5a75f678312cca98f
SUNGIL KIM <sonny0204@partron.co.kr>
2025-04-23 10:31:18 +0900

    object 수정

--- presenter/main/src/main/java/com/sungil/main/MainViewModel.kt
+++ presenter/main/src/main/java/com/sungil/main/MainViewModel.kt
@@ -141,7 +141,7 @@ class MainViewModel @Inject constructor(
     }
 
     sealed interface UiState<out T> {
-        object Loading : UiState<Nothing>
+        data object Loading : UiState<Nothing>
         data class Success<T>(val data: T) : UiState<T>
         data class Error(val message: String) : UiState<Nothing>
     }
