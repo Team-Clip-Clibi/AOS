70f791775a8c537cfd3e89c8866b0b28188f51f2
SUNGIL KIM <sonny0204@partron.co.kr>
2025-04-25 16:47:54 +0900

    PAGING 포팅 완료

--- data/src/main/java/com/example/data/paging/NotificationPagingSource.kt
+++ data/src/main/java/com/example/data/paging/NotificationPagingSource.kt
@@ -2,8 +2,11 @@ package com.example.data.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.example.data.mapper.toLoadResult
 import com.sungil.database.token.TokenManager
 import com.sungil.domain.TOKEN_FORM
+import com.sungil.domain.exception.TokenSaveException
+import com.sungil.domain.exception.UnauthorizedException
 import com.sungil.domain.model.Notification
 import com.sungil.network.http.HttpApi
 import retrofit2.HttpException
@@ -11,42 +14,58 @@ import javax.inject.Inject
 
 class NotificationPagingSource @Inject constructor(
     private val api: HttpApi,
-    private val tokenManger: TokenManager,
+    private val tokenManager: TokenManager,
 ) : PagingSource<String, Notification>() {
+
     override fun getRefreshKey(state: PagingState<String, Notification>): String? = null
 
     override suspend fun load(params: LoadParams<String>): LoadResult<String, Notification> {
         return try {
-            val token = tokenManger.getToken()
-            val response = if (params.key == null) {
-                api.requestNotify(TOKEN_FORM + token.first)
-            } else {
-                api.requestNewNotify(TOKEN_FORM + token.first, params.key!!)
+            val (accessToken, refreshToken) = tokenManager.getToken()
+            val response = when (params.key) {
+                null -> api.requestNotify(TOKEN_FORM + accessToken)
+                else -> api.requestNewNotify(TOKEN_FORM + accessToken, params.key!!)
             }
-            if (!response.isSuccessful) {
-                return LoadResult.Error(HttpException(response))
-            }
-            return when (response.code()) {
+
+            when (response.code()) {
+                200 -> response.toLoadResult()
                 401 -> {
-                    val newToken = api.requestRefreshToken(token.second)
-                    if(!newToken.isSuccessful){
-                        return LoadResult.Error(HttpException(newToken))
+                    val refreshResponse = api.requestRefreshToken(refreshToken)
+
+                    when {
+                        !refreshResponse.isSuccessful -> {
+                            LoadResult.Error(UnauthorizedException("serverError"))
+                        }
+
+                        refreshResponse.code() != 200 || refreshResponse.body() == null -> {
+                            LoadResult.Error(UnauthorizedException("reLogin"))
+                        }
+
+                        !tokenManager.updateToken(
+                            refreshResponse.body()!!.accessToken,
+                            refreshResponse.body()!!.refreshToken
+                        ) -> {
+                            LoadResult.Error(TokenSaveException("save error"))
+                        }
+
+                        else -> {
+                            val retry = when (params.key) {
+                                null -> api.requestNotify(TOKEN_FORM + refreshResponse.body()!!.accessToken)
+                                else -> api.requestNewNotify(TOKEN_FORM + refreshResponse.body()!!.accessToken, params.key!!)
+                            }
+
+                            if (retry.isSuccessful) {
+                                retry.toLoadResult()
+                            } else {
+                                LoadResult.Error(HttpException(retry))
+                            }
+                        }
                     }
-                    if(newToken.code() != 200){
-                        return LoadResult.Error(//TODO 해당 부분에서 사용자에게 재 로그인 하라는 메세지를 View에 전달해야함)
-                    }
-                    val saveTokenResult = tokenManger.updateToken(newToken.body()!!.accessToken , newToken.body()!!.refreshToken)
-                    if(!saveTokenResult){
-                        return LoadResult.Error(//TODO 사용자에게 앱 에러 상황을 알려야함)
-                    }
-                    val retry = if(params.key == null){
-                        api.requestNotify(TOKEN_FORM + newToken.body()!!.accessToken)
-                    }else {
-                        api.requestNewNotify(TOKEN_FORM + newToken.body()!!.accessToken, params.key!!)
-                    }
-                    return retry.toLoadResult()
                 }
+
+                else -> LoadResult.Error(HttpException(response))
             }
+
         } catch (e: Exception) {
             LoadResult.Error(e)
         }
---
+++ domain/src/main/java/com/sungil/domain/exception/NetworkException.kt
@@ -0,0 +1,4 @@
+package com.sungil.domain.exception
+
+class UnauthorizedException(message: String) : Exception(message)
+class TokenSaveException(message: String) : Exception(message)
