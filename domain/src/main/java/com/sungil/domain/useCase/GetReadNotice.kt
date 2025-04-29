236196f82dd1d356f84a7c601c165f7c4fc193c9
sonny8569 <sonny132@naver.com>
2025-04-29 22:13:46 +0900

    비즈니스 로직 개발

---
+++ domain/src/main/java/com/sungil/domain/useCase/GetReadNotice.kt
@@ -0,0 +1,13 @@
+package com.sungil.domain.useCase
+
+import androidx.paging.PagingData
+import com.sungil.domain.model.Notification
+import com.sungil.domain.repository.NetworkRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetReadNotice @Inject constructor(private val network: NetworkRepository) {
+    suspend fun invoke(): Flow<PagingData<Notification>> {
+        return network.requestReadNotificationPaging()
+    }
+}
