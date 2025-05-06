e1e2c943f113d421277ba19c7a437e1916032cd3
sonny8569 <sonny132@naver.com>
2025-05-07 00:20:37 +0900

    마지막까지 호출하고 새로운 알람 호출 안하는 버그 있음

--- domain/src/main/java/com/sungil/domain/model/Notification.kt
+++ domain/src/main/java/com/sungil/domain/model/Notification.kt
@@ -1,7 +1,9 @@
 package com.sungil.domain.model
 
 import java.time.Duration
+import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
@@ -15,17 +17,19 @@ data class Notification(
         get() = createdAt.toFriendlyTime()
 
     private fun String.toFriendlyTime(): String {
-        val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
-        val time = ZonedDateTime.parse(this, formatter)
-        val now = ZonedDateTime.now(ZoneId.systemDefault())
+        val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+        val localTime = LocalDateTime.parse(this, formatter)
 
-        val duration = Duration.between(time, now)
+        val zonedTime = localTime.atZone(ZoneOffset.UTC).withZoneSameInstant(ZoneId.systemDefault())
+
+        val now = ZonedDateTime.now(ZoneId.systemDefault())
+        val duration = Duration.between(zonedTime, now)
 
         return when {
             duration.toMinutes() < 1 -> "방금 전"
             duration.toMinutes() < 60 -> "${duration.toMinutes()}분 전"
             duration.toHours() < 24 -> "${duration.toHours()}시간 전"
-            else -> time.toLocalDate().format(DateTimeFormatter.ofPattern("M월 d일"))
+            else -> zonedTime.toLocalDate().format(DateTimeFormatter.ofPattern("M월 d일"))
         }
     }
 }
