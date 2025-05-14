14aa1a723f4b07507fb03f6c096102e62c3efb11
sonny8569 <sonny132@naver.com>
2025-05-14 22:41:00 +0900

    토스 xml 추가

---
+++ billing/src/main/java/com/sungil/billing/BillingActivity.kt
@@ -0,0 +1,11 @@
+package com.sungil.billing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BillingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
