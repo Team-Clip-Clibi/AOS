1c400c0b25bdd83fe022afa6b07bd9f1af63cc48
SUNGIL KIM <sonny0204@partron.co.kr>
2025-05-15 17:30:38 +0900

    네비게이션바 영역 침투 수정

--- billing/src/main/java/com/sungil/billing/BillingActivity.kt
+++ billing/src/main/java/com/sungil/billing/BillingActivity.kt
@@ -3,7 +3,9 @@ package com.sungil.billing
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import com.sungil.billing.databinding.ActivityBlillingBinding
 import com.tosspayments.paymentsdk.PaymentWidget
 import com.tosspayments.paymentsdk.model.PaymentCallback
@@ -18,7 +20,11 @@ class BillingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBlillingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        WindowCompat.setDecorFitsSystemWindows(window, true)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.billing_activity)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         init()
         addListener()
     }
