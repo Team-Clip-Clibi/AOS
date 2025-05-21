a4e3052ca953ab71e95ffd7e646a9d5ccdf53405
SUNGIL KIM <sonny0204@partron.co.kr>
2025-05-21 15:58:28 +0900

    초기매칭 정보 업데이트 로직 구현(respose별 대응 필여)

--- presenter/main/first_match/src/main/java/com/oneThing/first_matrch/FirstMatchViewModel.kt
+++ presenter/main/first_match/src/main/java/com/oneThing/first_matrch/FirstMatchViewModel.kt
@@ -3,7 +3,11 @@ package com.oneThing.first_matrch
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sungil.domain.useCase.GetUserInfo
+import com.sungil.domain.useCase.UpdateDiet
+import com.sungil.domain.useCase.UpdateJob
+import com.sungil.domain.useCase.UpdateLanguage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,6 +18,9 @@ import javax.inject.Inject
 @HiltViewModel
 class FirstMatchViewModel @Inject constructor(
     private val userInfo: GetUserInfo,
+    private val job: UpdateJob,
+    private val diet: UpdateDiet,
+    private val language: UpdateLanguage,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(FirstMatchData())
     val uiState: StateFlow<FirstMatchData> = _uiState.asStateFlow()
@@ -78,6 +85,64 @@ class FirstMatchViewModel @Inject constructor(
         }
     }
 
+    fun updateJob() {
+        viewModelScope.launch(Dispatchers.IO) {
+            when (val result = job.invoke(
+                UpdateJob.Param(
+                    Pair(_uiState.value.job.elementAt(0).name, _uiState.value.job.elementAt(1).name)
+                )
+            )) {
+                is UpdateJob.Result.Fail -> {
+                    _uiState.update { current ->
+                        current.copy(error = UiError.Error(result.errorMessage))
+                    }
+                }
+
+                is UpdateJob.Result.Success -> {
+                    _uiState.update { current ->
+                        current.copy(success = UiSuccess.Success(result.message))
+                    }
+                }
+            }
+        }
+    }
+
+    fun updateDiet() {
+        viewModelScope.launch(Dispatchers.IO) {
+            when (val result = diet.invoke(UpdateDiet.Param(_uiState.value.diet))) {
+                is UpdateDiet.Result.Fail -> {
+                    _uiState.update { current ->
+                        current.copy(error = UiError.Error(result.errorMessage))
+                    }
+                }
+
+                is UpdateDiet.Result.Success -> {
+                    _uiState.update { current ->
+                        current.copy(success = UiSuccess.Success(result.message))
+                    }
+                }
+            }
+        }
+    }
+
+    fun updateLanguage() {
+        viewModelScope.launch(Dispatchers.IO) {
+            when (val result = language.invoke(UpdateLanguage.Param(_uiState.value.language))) {
+                is UpdateLanguage.Result.Fail -> {
+                    _uiState.update { current ->
+                        current.copy(error = UiError.Error(result.errorMessage))
+                    }
+                }
+
+                is UpdateLanguage.Result.Success -> {
+                    _uiState.update { current ->
+                        current.copy(success = UiSuccess.Success(result.message))
+                    }
+                }
+            }
+        }
+    }
+
     fun diet(data: String) {
         _uiState.update { current ->
             current.copy(diet = data)
@@ -104,9 +169,15 @@ data class FirstMatchData(
     val diet: String = DIET.NONE.displayName,
     val language: String = "",
     val error: UiError = UiError.None,
+    val success: UiSuccess = UiSuccess.None,
 )
 
 sealed class UiError {
     data object None : UiError()
     data class Error(val message: String) : UiError()
+}
+
+sealed class UiSuccess {
+    data object None : UiSuccess()
+    data class Success(val message: String) : UiSuccess()
 }
