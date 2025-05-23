997a1ef40805ac66662df4c97e55682a2eca7d00
SUNGIL KIM <sonny0204@partron.co.kr>
2025-05-23 16:35:59 +0900

    직업 변경 view 디자인 시스템 적용

--- presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeJob/ChangeJobMainView.kt
+++ presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeJob/ChangeJobMainView.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
@@ -42,33 +44,37 @@ internal fun ChangeJobMainView(
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val context = LocalContext.current
-    LaunchedEffect(uiState.error , uiState.success) {
-        when(val error = uiState.error){
+    LaunchedEffect(uiState.error, uiState.success) {
+        when (val error = uiState.error) {
             is UiError.Error -> {
-                when(error.message){
+                when (error.message) {
                     ERROR_NETWORK -> {
                         snackBarHost.showSnackbar(
                             message = context.getString(R.string.txt_network_error),
                             duration = SnackbarDuration.Short
                         )
                     }
+
                     ERROR_FAIL_SAVE -> {
                         snackBarHost.showSnackbar(
                             message = context.getString(R.string.msg_save_error),
                             duration = SnackbarDuration.Short
                         )
                     }
-                    ERROR_NONE_DATA_SELECT ->{
+
+                    ERROR_NONE_DATA_SELECT -> {
                         snackBarHost.showSnackbar(
                             message = context.getString(R.string.msg_select_data),
                             duration = SnackbarDuration.Short
                         )
                     }
                 }
+                viewModel.initSuccessError()
             }
+
             UiError.None -> Unit
         }
-        when(uiState.success){
+        when (uiState.success) {
             UiSuccess.None -> Unit
             is UiSuccess.Success -> {
                 snackBarHost.showSnackbar(
@@ -79,46 +85,27 @@ internal fun ChangeJobMainView(
             }
         }
     }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(
                 top = paddingValues.calculateTopPadding() + 32.dp,
-                bottom = 8.dp
+                start = 17.dp,
+                end = 16.dp,
+                bottom = paddingValues.calculateBottomPadding()
             )
-            .navigationBarsPadding()
+            .verticalScroll(rememberScrollState())
     ) {
-
-        Column(
-            modifier = Modifier
-                .padding(start = 17.dp, end = 16.dp)
-                .weight(1f)
-        ) {
-            Text(
-                text = stringResource(R.string.txt_job_select_one),
-                color = ColorStyle.GRAY_600,
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            JobGridSelector(
-                selectedJobs = JOB.fromDisplayName(uiState.job),
-                onJobToggle = viewModel::setJob,
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-
-        HorizontalDivider(thickness = 1.dp, color = Color(0xFFEFEFEF))
-        Spacer(modifier = Modifier.height(8.dp))
-        ButtonXXLPurple400(
-            onClick = { viewModel.changeJob() },
-            buttonText = stringResource(R.string.btn_finish),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(60.dp),
-            isEnable = uiState.buttonRun
+        Text(
+            text = stringResource(R.string.txt_job_select_one),
+            color = ColorStyle.GRAY_600,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        JobGridSelector(
+            selectedJobs = JOB.fromDisplayName(uiState.job),
+            onJobToggle = viewModel::setJob,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
--- presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeJob/ChangeJobView.kt
+++ presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeJob/ChangeJobView.kt
@@ -3,20 +3,27 @@ package com.sungil.editprofile.ui.changeJob
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.core.ButtonXXLPurple400
+import com.example.core.ColorStyle
+import com.example.core.CustomSnackBar
+import com.example.core.TopAppBarNumber
 import com.sungil.editprofile.ProfileEditViewModel
 import com.sungil.editprofile.R
-import com.sungil.editprofile.ui.CustomChangeDataAppBar
-import com.sungil.editprofile.ui.CustomSnackBar
+
 
 @Composable
 internal fun ChangeJobView(
@@ -24,12 +31,16 @@ internal fun ChangeJobView(
     onBackClick: () -> Unit,
 ) {
     val snackBarHostState = remember { SnackbarHostState() }
+    val uiState by viewModel.uiState.collectAsState()
     Scaffold(
         topBar = {
-            CustomChangeDataAppBar(
-                text = stringResource(R.string.top_job_change_job),
+            TopAppBarNumber(
+                title = stringResource(R.string.top_job_change_job),
+                currentPage = 0,
+                totalPage = 0,
+                isPageTextShow = false,
                 onBackClick = {
-                    viewModel.initFlow()
+                    viewModel.initSuccessError()
                     onBackClick()
                 }
             )
@@ -48,7 +59,20 @@ internal fun ChangeJobView(
                             .calculateTopPadding() + 70.dp
                     )
             )
-        }
+        },
+        bottomBar = {
+            HorizontalDivider(thickness = 1.dp, color = ColorStyle.GRAY_200)
+            ButtonXXLPurple400(
+                onClick = viewModel::changeJob,
+                buttonText = stringResource(R.string.btn_finish),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .padding(start = 17.dp, end = 16.dp),
+                isEnable = uiState.buttonRun
+            )
+        },
+        contentColor = ColorStyle.WHITE_100
     ) { paddingValues ->
         ChangeJobMainView(
             paddingValues = paddingValues,
