80b27c7bd38d847ab4bc217d5ab87581d40825ca
SUNGIL KIM <sonny0204@partron.co.kr>
2025-05-23 17:16:25 +0900

    언어 상태 변경 UI 디자인 시스템 적용

--- presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeLanguage/ChangeLanguageMainView.kt
+++ presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeLanguage/ChangeLanguageMainView.kt
@@ -1,34 +1,24 @@
 package com.sungil.editprofile.ui.changeLanguage
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.core.ButtonLeftLarge
-import com.example.core.ButtonXXLPurple400
 import com.sungil.editprofile.ERROR_FAIL_SAVE
 import com.sungil.editprofile.ERROR_NETWORK
 import com.sungil.editprofile.ERROR_TOKEN_NULL
@@ -38,8 +28,6 @@ import com.sungil.editprofile.ProfileEditViewModel
 import com.sungil.editprofile.R
 import com.sungil.editprofile.UiError
 import com.sungil.editprofile.UiSuccess
-import com.sungil.editprofile.ui.CustomButton
-import com.sungil.editprofile.ui.CustomItemPick
 
 @Composable
 internal fun ChangeLanguageMainView(
@@ -49,8 +37,8 @@ internal fun ChangeLanguageMainView(
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val context = LocalContext.current
-    LaunchedEffect(uiState.success , uiState.error) {
-        when(val error = uiState.error){
+    LaunchedEffect(uiState.success, uiState.error) {
+        when (val error = uiState.error) {
             is UiError.Error -> {
                 when (error.message) {
                     ERROR_TOKEN_NULL -> {
@@ -59,18 +47,21 @@ internal fun ChangeLanguageMainView(
                             duration = SnackbarDuration.Short
                         )
                     }
+
                     ERROR_USER_DATA_NULL -> {
                         snackBarHost.showSnackbar(
                             message = context.getString(R.string.msg_user_data_null),
                             duration = SnackbarDuration.Short
                         )
                     }
+
                     ERROR_NETWORK -> {
                         snackBarHost.showSnackbar(
                             message = context.getString(R.string.txt_network_error),
                             duration = SnackbarDuration.Short
                         )
                     }
+
                     ERROR_FAIL_SAVE -> {
                         snackBarHost.showSnackbar(
                             message = context.getString(R.string.msg_save_error),
@@ -79,9 +70,10 @@ internal fun ChangeLanguageMainView(
                     }
                 }
             }
+
             UiError.None -> Unit
         }
-        when(uiState.success){
+        when (uiState.success) {
             UiSuccess.None -> Unit
             is UiSuccess.Success -> {
                 snackBarHost.showSnackbar(
@@ -92,64 +84,39 @@ internal fun ChangeLanguageMainView(
             }
         }
     }
-    val scrollState = rememberScrollState()
-
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(
                 top = paddingValues.calculateTopPadding() + 32.dp,
-                bottom = 8.dp
+                bottom = paddingValues.calculateBottomPadding(),
+                start = 17.dp,
+                end = 16.dp
             )
-            .navigationBarsPadding()
-            .verticalScroll(scrollState)
+            .verticalScroll(rememberScrollState())
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.TopCenter)
-                .padding(start = 17.dp, end = 16.dp)
-        ) {
-            ButtonLeftLarge(
-                text = stringResource(R.string.txt_language_kor),
-                isSelected = uiState.language == LANGUAGE.KOREAN.name,
-                onClick = {
-                    viewModel.setLanguage(LANGUAGE.KOREAN)
-                }
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            ButtonLeftLarge(
-                text = stringResource(R.string.txt_language_eng),
-                isSelected = uiState.language == LANGUAGE.ENGLISH.name,
-                onClick = {
-                    viewModel.setLanguage(LANGUAGE.ENGLISH)
-                }
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            ButtonLeftLarge(
-                text = stringResource(R.string.txt_language_both),
-                isSelected = uiState.language == LANGUAGE.BOTH.name,
-                onClick = {
-                    viewModel.setLanguage(LANGUAGE.BOTH)
-                }
-            )
-            Spacer(modifier = Modifier.weight(1f))
-        }
-        Column(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth()
-        ) {
-            HorizontalDivider(thickness = 1.dp, color = Color(0xFFEFEFEF))
-            Spacer(modifier = Modifier.height(8.dp))
-            ButtonXXLPurple400(
-                onClick = { viewModel.changeLanguage() },
-                buttonText = stringResource(R.string.btn_finish),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(60.dp),
-                isEnable = uiState.buttonRun
-            )
-        }
+        ButtonLeftLarge(
+            text = stringResource(R.string.txt_language_kor),
+            isSelected = uiState.language == LANGUAGE.KOREAN.name,
+            onClick = {
+                viewModel.setLanguage(LANGUAGE.KOREAN)
+            }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonLeftLarge(
+            text = stringResource(R.string.txt_language_eng),
+            isSelected = uiState.language == LANGUAGE.ENGLISH.name,
+            onClick = {
+                viewModel.setLanguage(LANGUAGE.ENGLISH)
+            }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonLeftLarge(
+            text = stringResource(R.string.txt_language_both),
+            isSelected = uiState.language == LANGUAGE.BOTH.name,
+            onClick = {
+                viewModel.setLanguage(LANGUAGE.BOTH)
+            }
+        )
     }
 }
--- presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeLanguage/ChangeLanguageView.kt
+++ presenter/main/editProfile/src/main/java/com/sungil/editprofile/ui/changeLanguage/ChangeLanguageView.kt
@@ -1,22 +1,30 @@
 package com.sungil.editprofile.ui.changeLanguage
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.core.ButtonXXLPurple400
+import com.example.core.ColorStyle
+import com.example.core.CustomSnackBar
+import com.example.core.TopAppBarNumber
 import com.sungil.editprofile.ProfileEditViewModel
 import com.sungil.editprofile.R
-import com.sungil.editprofile.ui.CustomChangeDataAppBar
-import com.sungil.editprofile.ui.CustomSnackBar
 
 @Composable
 internal fun ChangeLanguageView(
@@ -24,10 +32,14 @@ internal fun ChangeLanguageView(
     onBackClick: () -> Unit,
 ) {
     val snackBarHostState = remember { SnackbarHostState() }
+    val uiState by viewModel.uiState.collectAsState()
     Scaffold(
         topBar = {
-            CustomChangeDataAppBar(
-                text = stringResource(R.string.top_language),
+            TopAppBarNumber(
+                title = stringResource(R.string.top_language),
+                currentPage = 0,
+                totalPage = 0,
+                isPageTextShow = false,
                 onBackClick = {
                     viewModel.initSuccessError()
                     onBackClick()
@@ -43,12 +55,29 @@ internal fun ChangeLanguageView(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(
-                        start = 17.dp, end = 16.dp, bottom = WindowInsets.navigationBars
+                        start = 17.dp, end = 16.dp,
+                        bottom = WindowInsets.navigationBars
                             .asPaddingValues()
                             .calculateTopPadding() + 70.dp
                     )
             )
-        }
+        },
+        bottomBar = {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                HorizontalDivider(thickness = 1.dp, color = ColorStyle.GRAY_200)
+                Spacer(modifier = Modifier.height(8.dp))
+                ButtonXXLPurple400(
+                    onClick = viewModel::changeLanguage,
+                    buttonText = stringResource(R.string.btn_finish),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .padding(start = 17.dp , end = 16.dp),
+                    isEnable = uiState.buttonRun
+                )
+            }
+        },
+        contentColor = ColorStyle.WHITE_100
     ) { paddingValues ->
         ChangeLanguageMainView(
             viewModel = viewModel,
