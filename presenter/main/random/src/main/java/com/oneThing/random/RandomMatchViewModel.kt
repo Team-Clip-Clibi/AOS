7fd0fe4df536caa4af185e6fee361f7c471dc8dd
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-02 16:23:11 +0900

    랜덤 매칭 viewModel 생성

---
+++ presenter/main/random/src/main/java/com/oneThing/random/RandomMatchViewModel.kt
@@ -0,0 +1,9 @@
+package com.oneThing.random
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RandomMatchViewModel @Inject constructor() : ViewModel() {
+}
