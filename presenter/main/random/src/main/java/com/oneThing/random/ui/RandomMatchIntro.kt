7c932aa42090e3ac44d34e0217134632ba3170cb
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-02 17:18:04 +0900

    intro view 개발 중

---
+++ presenter/main/random/src/main/java/com/oneThing/random/ui/RandomMatchIntro.kt
@@ -0,0 +1,11 @@
+package com.oneThing.random.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun RandomMatchIntro(
+    onBack: () -> Unit,
+    onNext: () -> Unit,
+) {
+
+}
