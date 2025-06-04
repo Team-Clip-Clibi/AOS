2138ffe5a2a7efd8afe301b297fc3543c7e559a1
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-04 13:43:51 +0900

    usecase error message 정의

--- presenter/main/random/src/main/java/com/oneThing/random/component/_define.kt
+++ presenter/main/random/src/main/java/com/oneThing/random/component/_define.kt
@@ -1,3 +1,6 @@
 package com.oneThing.random.component
 
 const val NAV_RANDOM_MATCH_INTRO = "randomMatchIntro"
+
+const val ERROR_NETWORK_ERROR = "network Error"
+const val ERROR_RE_LOGIN = "reLogin"
