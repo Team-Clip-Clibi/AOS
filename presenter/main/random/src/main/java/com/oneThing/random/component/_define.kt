4e92a7add1ab4c52280ed36212f201a70631be46
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-05 12:02:15 +0900

    ERROR 메세지 정의

--- presenter/main/random/src/main/java/com/oneThing/random/component/_define.kt
+++ presenter/main/random/src/main/java/com/oneThing/random/component/_define.kt
@@ -5,9 +5,15 @@ const val NAV_RANDOM_MATCH_DUPLICATE = "randomMatchDuplicate"
 const val NAV_RANDOM_LOCATION = "randomLocation"
 const val NAV_RANDOM_TMI = "randomTmi"
 const val NAV_RANDOM_TOPIC = "randomTopic"
+const val NAV_RANDOM_BEFORE_PAY = "randomBeforePay"
+const val NAV_HOME = "Main"
+const val NAV_LOGIN = "Login"
+const val NAV_PAY = "pay"
 
 const val ERROR_NETWORK_ERROR = "network Error"
 const val ERROR_RE_LOGIN = "reLogin"
+const val ERROR_RANDOM_MATCH_FAIL = "Random Match Error"
+const val ERROR_TOSS_NOT_INSTALL = "not install toss"
 
 const val NEXT_DATE_EMPTY = ""
 
