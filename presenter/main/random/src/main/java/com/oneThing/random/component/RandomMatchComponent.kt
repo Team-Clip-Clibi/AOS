2ddf6b0a7927ae4371b6cb7821d9f28be9ff688d
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-05 12:05:32 +0900

    랜덤매칭 정보 view 개발 완료

--- presenter/main/random/src/main/java/com/oneThing/random/component/RandomMatchComponent.kt
+++ presenter/main/random/src/main/java/com/oneThing/random/component/RandomMatchComponent.kt
@@ -8,18 +8,23 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,7 +34,10 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.core.AppTextStyles
 import com.example.core.ButtonXXLPurple400
@@ -180,3 +188,125 @@ fun SlidingTextBox(textList: List<String>) {
         }
     }
 }
+
+@Composable
+fun EventView(
+    title: String,
+    content: String,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .height(68.dp)
+            .background(color = ColorStyle.WHITE_100, shape = RoundedCornerShape(size = 12.dp))
+            .padding(start = 16.dp, end = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_discount),
+            contentDescription = "discount",
+            tint = Color.Unspecified,
+            modifier = Modifier.size(36.dp)
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = title,
+                style = AppTextStyles.CAPTION_12_18_SEMI,
+                color = ColorStyle.PURPLE_400
+            )
+            Text(
+                text = content,
+                style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                color = ColorStyle.GRAY_800
+            )
+        }
+    }
+}
+
+@Composable
+fun RandomMatchDataView(
+    nickName: String,
+    time: String,
+    location: String,
+    address: String,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = ColorStyle.WHITE_100, shape = RoundedCornerShape(size = 16.dp))
+            .padding(top = 18.dp, start = 16.dp, end = 16.dp, bottom = 18.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.random_before_pay_content_title, nickName),
+            style = AppTextStyles.SUBTITLE_16_24_SEMI,
+            color = ColorStyle.GRAY_800,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_time),
+                    contentDescription = "time icon",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = stringResource(R.string.random_before_pay_time),
+                    style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                    color = ColorStyle.GRAY_800
+                )
+            }
+            Text(
+                text = time,
+                style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                color = ColorStyle.GRAY_800
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_location),
+                    contentDescription = "location icon",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = stringResource(R.string.random_before_pay_time),
+                    style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                    color = ColorStyle.GRAY_800
+                )
+            }
+            Column {
+                Text(
+                    text = location,
+                    style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                    color = ColorStyle.GRAY_800
+                )
+                Text(
+                    text = address,
+                    style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                    color = ColorStyle.GRAY_800
+                )
+            }
+        }
+    }
+}
+
