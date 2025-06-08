adfc9ab0e9f2fd876abff2c668991e54bb017525
sonny8569 <sonny132@naver.com>
2025-06-08 21:26:45 +0900

    1.0.7 테스트 버전 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
