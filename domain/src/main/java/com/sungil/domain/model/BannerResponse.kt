41feb0c4078397425304679722cf8c2999ba30dd
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-09 16:17:32 +0900

    변수 nullable 사유 -> 서버에서 null값 return 대응

--- domain/src/main/java/com/sungil/domain/model/BannerResponse.kt
+++ domain/src/main/java/com/sungil/domain/model/BannerResponse.kt
@@ -1,12 +1,8 @@
 package com.sungil.domain.model
 
-data class BannerResponse(
-    val responseCode: Int,
-    val bannerResponse : List<BannerData>
-)
 
 data class BannerData(
     val image: String,
-    val headText: String,
-    val subText: String,
+    val headText: String?,
+    val subText: String?,
 )
