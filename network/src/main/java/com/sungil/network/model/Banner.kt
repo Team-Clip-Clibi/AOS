4777bebc5ad73aa17e4d624903f225b82e8fd2dc
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-09 16:14:45 +0900

    배터 변수 nullalbe로 지정 사유 : 서버에서 null값 return 대응

--- network/src/main/java/com/sungil/network/model/Banner.kt
+++ network/src/main/java/com/sungil/network/model/Banner.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Banner(
-    val imagePresignedUrl: String,
-    val headText: String,
-    val subText: String,
+    val imagePresignedUrl: String?,
+    val headText: String?,
+    val subText: String?,
 )
