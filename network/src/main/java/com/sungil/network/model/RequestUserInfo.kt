6c90d7b91b5d3613d1543ae91d1629353b122af7
sonny8569 <sonny132@naver.com>
2025-06-11 00:07:38 +0900

    nullable 추가

--- network/src/main/java/com/sungil/network/model/RequestUserInfo.kt
+++ network/src/main/java/com/sungil/network/model/RequestUserInfo.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RequestUserInfo(
-    val username: String,
+    val username: String?,
     val platform: String,
-    val phoneNumber: String,
+    val phoneNumber: String?,
     val nickname: String?,
 )
