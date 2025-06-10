4794fb41f0b4941311fd9bb0786dfe7f3d6759e5
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-10 12:08:54 +0900

    불필요 import 삭제

--- presenter/pay_finish/src/main/java/com/sungil/pay_finish/ui/PayFinishView.kt
+++ presenter/pay_finish/src/main/java/com/sungil/pay_finish/ui/PayFinishView.kt
@@ -12,15 +12,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
