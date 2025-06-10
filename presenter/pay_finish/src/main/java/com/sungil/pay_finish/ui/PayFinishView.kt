40625be6f428c839c81cdc7a23880a1ee897aab6
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-10 12:05:33 +0900

    결제 완료 이미지 추가

--- presenter/pay_finish/src/main/java/com/sungil/pay_finish/ui/PayFinishView.kt
+++ presenter/pay_finish/src/main/java/com/sungil/pay_finish/ui/PayFinishView.kt
@@ -1,6 +1,8 @@
 package com.sungil.pay_finish.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,8 +18,10 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.core.AppTextStyles
@@ -88,26 +92,41 @@ internal fun PayFinishView(
             }
         }
     ) { paddingValues ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
+                .background(color = ColorStyle.WHITE_100)
                 .padding(
                     top = paddingValues.calculateTopPadding() + 32.dp,
                     start = 17.dp,
-                    end = 16.dp
+                    end = 16.dp,
+                    bottom = paddingValues.calculateBottomPadding()
                 )
-                .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                text = stringResource(R.string.txt_title),
-                style = AppTextStyles.HEAD_28_40_BOLD,
-                color = ColorStyle.GRAY_800
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = stringResource(R.string.txt_sub_title),
-                style = AppTextStyles.SUBTITLE_16_24_SEMI,
-                color = ColorStyle.GRAY_600
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.TopCenter)
+            ) {
+                Text(
+                    text = stringResource(R.string.txt_title),
+                    style = AppTextStyles.HEAD_28_40_BOLD,
+                    color = ColorStyle.GRAY_800
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = stringResource(R.string.txt_sub_title),
+                    style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                    color = ColorStyle.GRAY_600
+                )
+            }
+            Image(
+                painter = painterResource(R.drawable.ic_pay_success),
+                contentDescription = "pay success image",
+                modifier = Modifier
+                    .width(148.dp)
+                    .height(148.dp)
+                    .align(Alignment.Center)
             )
         }
     }
