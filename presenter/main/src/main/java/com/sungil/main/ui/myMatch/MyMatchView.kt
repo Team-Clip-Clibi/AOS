737472415c1cced401ece6a83249ec92d9dcef4a
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-13 17:30:27 +0900

    내모임 개발중

--- presenter/main/src/main/java/com/sungil/main/ui/myMatch/MyMatchView.kt
+++ presenter/main/src/main/java/com/sungil/main/ui/myMatch/MyMatchView.kt
@@ -1,33 +1,56 @@
 package com.sungil.main.ui.myMatch
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.SecondaryIndicator
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.example.core.AppTextStyles
 import com.example.core.ColorStyle
 import com.example.core.CustomSnackBar
 import com.sungil.domain.model.UserData
 import com.sungil.main.ERROR_NETWORK_ERROR
+import com.sungil.main.LatestDayInfo
 import com.sungil.main.MainViewModel
+import com.sungil.main.MyMatchDestination
 import com.sungil.main.R
 import com.sungil.main.component.CustomMainPageTopBar
 import com.sungil.onethingmatch.ERROR_RE_LOGIN
@@ -38,6 +61,9 @@ fun MyMatchView(viewModel: MainViewModel, login: () -> Unit) {
     val state by viewModel.userState.collectAsState()
     val latestDay = state.latestDay
     val context = LocalContext.current
+    val navController = rememberNavController()
+    var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
+    val tab = MyMatchDestination.entries
     val userName = when (state.userDataState) {
         is MainViewModel.UiState.Success -> (state.userDataState as MainViewModel.UiState.Success<UserData>).data.nickName
         is MainViewModel.UiState.Loading -> ""
@@ -100,51 +126,175 @@ fun MyMatchView(viewModel: MainViewModel, login: () -> Unit) {
                     .background(color = ColorStyle.WHITE_100)
                     .padding(top = 16.dp, start = 17.dp, end = 16.dp)
             ) {
-                when (latestDay) {
-                    is MainViewModel.UiState.Error -> {
-                        Text(
-                            text = stringResource(R.string.my_match_title_not_approve , userName ?: "error"),
-                            style = AppTextStyles.TITLE_20_28_SEMI,
+                MyMatchTitleView(latestDay = latestDay, userName = userName)
+                Spacer(modifier = Modifier.height(32.dp))
+            }
+            Column(modifier = Modifier.fillMaxWidth().background(color = ColorStyle.WHITE_100)) {
+                ScrollableTabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    edgePadding = 0.dp,
+                    indicator = { tabPositions ->
+                        SecondaryIndicator(
+                            modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                            height = 3.dp,
                             color = ColorStyle.GRAY_800
                         )
-                    }
-
-                    is MainViewModel.UiState.Success -> {
-                        if (latestDay.data.applyTime == 0 && latestDay.data.confirmTime == 0) {
-                            Text(
-                                text = stringResource(R.string.my_match_title_not_apply),
-                                style = AppTextStyles.TITLE_20_28_SEMI,
-                                color = ColorStyle.GRAY_800
-                            )
-                            return@Scaffold
-                        }
-                        if (latestDay.data.applyTime != 0 && latestDay.data.confirmTime == 0) {
-                            Text(
-                                text = stringResource(
-                                    R.string.my_match_title_not_approve,
-                                    userName ?: "error"
-                                ),
-                                style = AppTextStyles.TITLE_20_28_SEMI,
-                                color = ColorStyle.GRAY_800
-                            )
-                            return@Scaffold
-                        }
-                        Text(
-                            text = stringResource(R.string.my_match_title, userName ?: "error"),
-                            style = AppTextStyles.HEAD_28_40_BOLD,
-                            color = ColorStyle.GRAY_800,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Text(
-                            text = stringResource(R.string.my_match_content, latestDay.data),
-                            style = AppTextStyles.HEAD_28_40_BOLD,
-                            color = ColorStyle.PURPLE_400,
-                            modifier = Modifier.fillMaxWidth()
+                    },
+                    containerColor = ColorStyle.WHITE_100
+                ) {
+                    tab.forEachIndexed { index, tab ->
+                        Tab(
+                            selected = selectedTabIndex == index,
+                            onClick = {
+                                selectedTabIndex = index
+                                //TODO NAV 미 구현
+                            },
+                            text = {
+                                Text(
+                                    text = tab.label,
+                                    style = AppTextStyles.SUBTITLE_16_24_SEMI,
+                                    color = when (selectedTabIndex) {
+                                        index -> ColorStyle.GRAY_800
+                                        else -> ColorStyle.GRAY_600
+                                    }
+                                )
+                            }
                         )
                     }
-                    else -> Unit
                 }
             }
         }
     }
 }
+
+@Composable
+fun MyMatchTitleView(
+    latestDay: MainViewModel.UiState<LatestDayInfo>,
+    userName: String?,
+) {
+    val name = userName ?: "error"
+
+    when (latestDay) {
+        is MainViewModel.UiState.Error -> {
+            Text(
+                text = stringResource(R.string.my_match_title_not_approve, name),
+                style = AppTextStyles.TITLE_20_28_SEMI,
+                color = ColorStyle.GRAY_800
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            MyMatchViewMatchInfo(
+                applyInfo = 0,
+                confirmInfo = 0
+            )
+        }
+
+        is MainViewModel.UiState.Success -> {
+            val apply = latestDay.data.applyTime
+            val confirm = latestDay.data.confirmTime
+
+            when {
+                apply == 0 && confirm == 0 -> {
+                    Text(
+                        text = stringResource(R.string.my_match_title_not_apply),
+                        style = AppTextStyles.TITLE_20_28_SEMI,
+                        color = ColorStyle.GRAY_800
+                    )
+                }
+
+                confirm == 0 -> {
+                    Text(
+                        text = stringResource(R.string.my_match_title_not_approve, name),
+                        style = AppTextStyles.TITLE_20_28_SEMI,
+                        color = ColorStyle.GRAY_800
+                    )
+                }
+
+                else -> {
+                    Text(
+                        text = stringResource(R.string.my_match_title, name),
+                        style = AppTextStyles.HEAD_28_40_BOLD,
+                        color = ColorStyle.GRAY_800,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = stringResource(R.string.my_match_content, latestDay.data),
+                        style = AppTextStyles.HEAD_28_40_BOLD,
+                        color = ColorStyle.PURPLE_400,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            MyMatchViewMatchInfo(
+                applyInfo = apply,
+                confirmInfo = confirm
+            )
+        }
+
+        else -> Unit
+    }
+}
+
+@Composable
+fun MyMatchViewMatchInfo(applyInfo: Int, confirmInfo: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(68.dp)
+            .background(color = ColorStyle.GRAY_100, shape = RoundedCornerShape(size = 8.dp))
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.my_match_content_apply),
+                style = AppTextStyles.CAPTION_12_18_SEMI,
+                color = ColorStyle.GRAY_600,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = applyInfo.toString(),
+                style = AppTextStyles.SUBTITLE_18_26_SEMI,
+                color = ColorStyle.GRAY_800,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+        VerticalDivider(
+            modifier = Modifier
+                .width(1.dp)
+                .fillMaxHeight()
+                .background(color = ColorStyle.GRAY_300)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.my_match_content_confirm),
+                style = AppTextStyles.CAPTION_12_18_SEMI,
+                color = ColorStyle.GRAY_600,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = confirmInfo.toString(),
+                style = AppTextStyles.SUBTITLE_18_26_SEMI,
+                color = ColorStyle.GRAY_800,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
