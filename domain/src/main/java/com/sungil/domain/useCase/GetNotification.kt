2ae8b179e6c2ef81812137ff5151a174d1be47db
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-18 15:30:12 +0900

    원띵 공지 link null 값 대응

--- domain/src/main/java/com/sungil/domain/useCase/GetNotification.kt
+++ domain/src/main/java/com/sungil/domain/useCase/GetNotification.kt
@@ -7,12 +7,13 @@ import com.sungil.domain.model.NotificationData
 import com.sungil.domain.repository.DatabaseRepository
 import com.sungil.domain.repository.NetworkRepository
 import com.sungil.domain.tokenManger.TokenMangerController
+import com.sungil.domain.useCase.GetLatestMatch.Result
 import javax.inject.Inject
 
 class GetNotification @Inject constructor(
     private val database: DatabaseRepository,
     private val network: NetworkRepository,
-    private val tokenManger : TokenMangerController
+    private val tokenManger: TokenMangerController,
 ) {
 
     sealed interface Result : UseCase.Result {
@@ -20,81 +21,33 @@ class GetNotification @Inject constructor(
         data class Fail(val errorMessage: String) : Result
     }
 
-    suspend fun invoke(): Result {
+    suspend fun invoke(): Any {
         val token = database.getToken()
-        return when(val notification = network.requestNotification(TOKEN_FORM + token.first)){
+        return when (val notification = network.requestNotification(TOKEN_FORM + token.first)) {
             is NetworkResult.Success -> {
                 Result.Success(notification.data.notificationDataList)
             }
 
-            is NetworkResult.Error -> Result.Fail("network error")
+            is NetworkResult.Error -> {
+                when (notification.code) {
+                    401 -> {
+                        val refreshToken = tokenManger.requestUpdateToken(token.second)
+                        if (!refreshToken) return Result.Fail("reLogin")
+                        val newToken = database.getToken()
+                        val reRequest = network.requestNotification(TOKEN_FORM + newToken.first)
+                        when (reRequest) {
+                            is NetworkResult.Success -> {
+                                return Result.Success(reRequest.data.notificationDataList)
+                            }
+
+                            is NetworkResult.Error -> {
+                                return Result.Fail("reLogin")
+                            }
+                        }
+                    }
+                    else -> return Result.Fail("network error")
+                }
+            }
         }
-//        when (notification.responseCode) {
-//            401 -> {
-//                val refreshToken = tokenManger.requestUpdateToken(token.second)
-//                if (!refreshToken) return Result.Fail("reLogin")
-//                val newToken = database.getToken()
-//                val retry = network.requestNotification(TOKEN_FORM + newToken.first)
-//                when (retry.responseCode) {
-//                    200 -> {
-//                        return Result.Success(retry.notificationDataList)
-//                    }
-//
-//                    204 -> {
-//                        /**
-//                         * TODO -> 배포시 서버 데이터로만 출력
-//                         */
-//                        return Result.Success(
-//                            listOf(
-//                                NotificationData(
-//                                    noticeType = "NOTICE",
-//                                    content = "테스트에용",
-//                                    link = "www.naver.com"
-//                                ),
-//                                NotificationData(
-//                                    noticeType = "ARTICLE",
-//                                    content = "테스트에용2",
-//                                    link = "www.naver.com"
-//                                ),
-//                                NotificationData(
-//                                    noticeType = "NOTICE",
-//                                    content = "테스트에용3",
-//                                    link = "www.naver.com"
-//                                ),
-//                            )
-//                        )
-//                    }
-//                    else -> return Result.Fail("network error")
-//                }
-//            }
-//
-//            200 -> {
-//                return Result.Success(notification.notificationDataList)
-//            }
-//
-//            204 -> {
-//                return Result.Success(
-//                    listOf(
-//                        NotificationData(
-//                            noticeType = "NOTICE",
-//                            content = "테스트에용",
-//                            link = "www.naver.com"
-//                        ),
-//                        NotificationData(
-//                            noticeType = "ARTICLE",
-//                            content = "테스트에용2",
-//                            link = "www.naver.com"
-//                        ),
-//                        NotificationData(
-//                            noticeType = "NOTICE",
-//                            content = "테스트에용3",
-//                            link = "www.naver.com"
-//                        ),
-//                    )
-//                )
-//            }
-//
-//            else -> return Result.Fail("network error")
-//        }
     }
 }
