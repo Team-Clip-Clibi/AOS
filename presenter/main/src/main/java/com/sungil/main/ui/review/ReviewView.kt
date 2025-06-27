fce87a382efcc06ec181b4eeee41fd18ebedb787
SUNGIL KIM <sonny0204@partron.co.kr>
2025-06-27 17:27:45 +0900

    리뷰 view 개발 중

---
+++ presenter/main/src/main/java/com/sungil/main/ui/review/ReviewView.kt
@@ -0,0 +1,8 @@
+package com.sungil.main.ui.review
+
+import androidx.compose.runtime.Composable
+import com.sungil.main.MainViewModel
+
+@Composable
+internal fun ReviewView(viewModel : MainViewModel) {
+}
