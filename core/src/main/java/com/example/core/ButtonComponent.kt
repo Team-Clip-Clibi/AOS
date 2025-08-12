223029e47982c0da78375eddb9eb15abe87522f6
SUNGIL KIM <sonny0204@partron.co.kr>
2025-08-12 15:48:28 +0900

    NXRB-52 수정

--- core/src/main/java/com/example/core/ButtonComponent.kt
+++ core/src/main/java/com/example/core/ButtonComponent.kt
@@ -98,8 +98,11 @@ fun ButtonPurple100MEDIUM(
 ) {
     Button(
         modifier = modifier
-            .height(48.dp)
-            .background(color = ColorStyle.PURPLE_400, shape = RoundedCornerShape(size = 12.dp)),
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = ColorStyle.PURPLE_400,
+            contentColor = ColorStyle.PURPLE_400
+        ),
         onClick = onClick,
         shape = RoundedCornerShape(8.dp),
     ) {
