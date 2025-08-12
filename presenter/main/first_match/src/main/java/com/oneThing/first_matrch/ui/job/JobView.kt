6cda4657177a797d8385e3ac00579d2c34c80df0
SUNGIL KIM <sonny0204@partron.co.kr>
2025-08-12 14:06:48 +0900

    NXRB-43 수정

--- presenter/main/first_match/src/main/java/com/oneThing/first_matrch/ui/job/JobView.kt
+++ presenter/main/first_match/src/main/java/com/oneThing/first_matrch/ui/job/JobView.kt
@@ -3,23 +3,16 @@ package com.oneThing.first_matrch.ui.job
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.example.core.AppTextStyles
 import com.example.core.ColorStyle
 import com.oneThing.first_matrch.Error
 import com.oneThing.first_matrch.FirstMatchViewModel
-import com.oneThing.first_matrch.R
 import com.oneThing.first_matrch.UiError
 import com.oneThing.first_matrch.component.JobGridSelector
 
